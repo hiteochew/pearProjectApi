9c0cf67c4d6870d826f4bcd7629d6c79110c4a14
小林囝 <dong@linyudong.cn>
2023-09-08 17:06:28 +0800

    修复php7.4下任务创建错误问题

--- config/session.php
+++ config/session.php
@@ -8,14 +8,14 @@ file_exists($session_path) || mkdir($session_path, 0755, true);
 /* 定义Session会话参数 */
 return [
     'id' => '',
-    'prefix' => 'ta',
+    'prefix' => 'project_',
     'type' => Env::get('cache.type', 'redis') == 'redis' ? 'redis' : '',
     'expire' => 3600 * 24,
     'auto_start' => true,
     'path' => $session_path,
     'name' => $session_name,
     'var_session_id' => $session_name,
-
+    'select' => 3,
 //    'use_trans_sid'  => 0,
 //    'httponly'       => true,
 //    'secure'         => true,
